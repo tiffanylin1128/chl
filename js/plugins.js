696998751bde685b5f271412b4d014065efb41fa
tiffany <coffeeanne26@gmail.com>
2019-03-12 11:39:26 +0800

    delete search
    delete search

--- js/plugins.js
+++ js/plugins.js
@@ -230,30 +230,5 @@ https://github.com/imakewebthings/jquery-waypoints/blob/master/licenses.txt
     };
 }(jQuery));
 
-// :: Search Area
-var search = document.getElementById('search'),
-    searchWrapper = document.getElementById('search-wrapper'),
-    closeIcon = document.getElementById('close-icon');
-    search.onfocus = function () {
-    searchWrapper.classList.add('search-expanded');
-    this.addEventListener('transitionend', function () {
-        closeIcon.style.display = 'block';
-    });
-}
-search.onblur = function () {
-    searchWrapper.classList.remove('search-expanded');
-    closeIcon.classList.add('closing');
-    this.addEventListener('transitionend', function () {
-        closeIcon.classList.remove('closing');
-        closeIcon.style.display = 'none';
-    });
-}
-closeIcon.onclick = function () {
-    this.classList.add('closing');
-    document.activeElement.blur();
-    setTimeout(function () {
-        closeIcon.classList.remove('closing');
-    }, 1000);
-}
 
 // [End Include All Plugins]
